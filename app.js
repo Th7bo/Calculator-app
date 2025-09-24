a3d249c1d3421717fe67f14153c4a090d2f7bc15
Th7bo <60064927+Th7bo@users.noreply.github.com>
2025-09-24 23:20:19 +0200

    Remove Node.js version check
    Removed Node.js version check for compatibility.

--- app.js
+++ app.js
@@ -3,12 +3,6 @@ const routes = require('./routes');
 
 const app = express();
 
-const NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
-if (NODE_MAJOR_VERSION != 14) {
-  throw new Error('You are using a version of NodeJS that is to high or to low. Please use NodeJS Version 14');
-}
-
-
 app.use(express.static('public'));
 app.use('/calc', routes);
 
